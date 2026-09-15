bfd761b55ffe47190a0f009e05d12233b5905eeb
Kush <kushneupane015@gmail.com>
2026-09-15 22:59:56 +0545

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+window.firebaseConfig = {
+    apiKey: "AIzaSyBl2DFwYuoeUjx3QokPP0gkG76XLPzhOak",
+    authDomain: "kush-website-6f5b3.firebaseapp.com",
+    projectId: "kush-website-6f5b3",
+    storageBucket: "kush-website-6f5b3.firebasestorage.app",
+    messagingSenderId: "13532622679",
+    appId: "1:13532622679:web:fb7b4ecb5274e7286dd812",
+    measurementId: "G-0RCP1JM4W2"
+};
